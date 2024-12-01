20760d682e3cf7563a2c5615e8b45fc5e0ffd842
Dusan <dule.pismo@gmail.com>
2024-12-01 01:01:02 +0100

    ws

--- server.js
+++ server.js
@@ -2,8 +2,32 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
 
 const port = 3000
+
+let playercount = 0;
+
+//ws
+wss.on('connection', (ws) => {
+  console.log('A new client connected.');
+  playercount++
+
+  // Event listener for incoming messages
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+        client.send(playercount);
+    }
+});
+
+  // Event listener for client disconnection
+  ws.on('close', () => {
+    console.log('A client disconnected.');
+    playercount--
+  });
+});
 
 //middlewares
 app.use(express.static(path.join(__dirname, 'public')));
